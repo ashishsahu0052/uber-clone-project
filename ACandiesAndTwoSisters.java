6d90664c84ee41225de515faa42218d8704944ff
Ashish Sahu <ashishsahu0052@gmail.com>
2026-08-29 23:38:58 +0530

    day 2 - backend

---
+++ ACandiesAndTwoSisters.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ACandiesAndTwoSisters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+
+            if (n <= 2) {
+                System.out.println(0);
+            } else if (n % 2 == 1) {
+                System.out.println(n / 2);
+            } else {
+                System.out.println(n / 2 - 1);
+            }
+        }
+    }
+}
